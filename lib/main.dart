323578e85969c7126ce56902e6331f9d162e2618
Beloin <beloin.rodrigues@gmail.com>
2019-11-07 02:54:05 -0300

    Branch master inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:serview/ui/home_page.dart';
 import 'package:serview/ui/sign_page.dart';
 
 void main() => runApp(MaterialApp(
-      home: SignPage(),
+      home: HomePage(),
     ));
