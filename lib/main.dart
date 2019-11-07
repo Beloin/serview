8cc63d61589259d8c6898be83aa3836ca2fa90bf
Beloin <beloin.rodrigues@gmail.com>
2019-11-07 03:01:00 -0300

    Atualização branch dev

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:serview/ui/home_page.dart';
 import 'package:serview/ui/sign_page.dart';
 
 void main() => runApp(MaterialApp(
-      home: HomePage(),
+      home: SignPage(),
     ));
