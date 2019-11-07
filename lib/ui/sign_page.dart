87864321cc6f484bd7d1dfc5e5666e5613961a6d
Beloin <beloin.rodrigues@gmail.com>
2019-11-07 14:22:42 -0300

    Atualização Branch SV-02

--- lib/ui/sign_page.dart
+++ lib/ui/sign_page.dart
@@ -6,6 +6,9 @@ class SignPage extends StatefulWidget {
 }
 
 class _SignPageState extends State<SignPage> {
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -29,22 +32,78 @@ class _SignPageState extends State<SignPage> {
           ),
           body: TabBarView(
             children: <Widget>[
+              //Primeira Tab
               Tab(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[Text("Aqui ficará a primeira aba do app")],
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        "Email:",
+                        style: TextStyle(color: Colors.blue, fontSize: 25.0),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: _buildFieldText(
+                          onSubmitted: _email,
+                          label: "Digite Aqui Seu Email",
+                          colorText: Colors.black),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        "Senha:",
+                        style: TextStyle(color: Colors.blue, fontSize: 25.0),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: _buildFieldText(
+                          onSubmitted: _password,
+                          label: "Digite Aqui Sua Senha",
+                          colorText: Colors.black,
+                          obscureText: true),
+                    ),
+                  ],
                 ),
               ),
+              // Segunda Tab
               Tab(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[Text("Aqui ficará a segunda aba do app")],
-                ),
-              )
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[]))
             ],
           ),
         ));
   }
-}
 
-//Criar método para a criação do TextField
+  Widget _buildFieldText(
+      {label = "TextField",
+      colorText = Colors.blue,
+      fontSize = 15.0,
+      onSubmitted, obscureText = false}) {
+    return TextField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.blue),
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.blue))),
+      onSubmitted: (text) {
+        onSubmitted = text;
+        print(onSubmitted);
+      },
+      style: TextStyle(fontSize: fontSize, color: colorText),
+      textAlign: TextAlign.left,
+    );
+  }
+}
