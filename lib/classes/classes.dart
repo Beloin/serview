fa710e2929ed218fc079ba5c48116422ede86e5b
Beloin <beloin.rodrigues@gmail.com>
2019-11-12 17:59:30 -0300

    Finalização classes

--- lib/classes/classes.dart
+++ lib/classes/classes.dart
@@ -6,9 +6,9 @@ class User {
   String _password;
   String _name;
   //img _fotoPerfil;
-  bool _Fornecedor = false;
+  bool _fornecedor = false;
   //Curriculo _curriculo;
-  double _classificacao = 3;
+  double _classification = 3;
 
   User(name, email, password){
     _password = password;
@@ -19,4 +19,30 @@ class User {
     if (idT < 100000) idT = idGen.nextInt(999999);
     _id = idT.toString();
   }
+  void msg(){}
+  void editPerfil(){}
+  void editClassification(){}
+}
+
+class Curriculum {
+  //Profession _prof;
+  String _description;
+  //List<img> _imgs;
+  Curriculum(description, /*prof,*/){
+    _description = description;
+    //_prof = prof;
+  }
+  void editProf(){}
+  void editDesc(){}
+}
+
+class Profession{
+  List<String> _prof;
+  String _currentProf;
+  
+  void editCurrentProf(){}
+  void addProf(){}
+  void rmvProf(){}
+  void searchProf(){}
+  void getCurrentProf(){}
 }
