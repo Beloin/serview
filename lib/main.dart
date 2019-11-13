becb5dc9586d0bf987f19470f249c44075542710
Beloin Sena <beloin.rodrigues@gmail.com>
2019-11-12 23:11:28 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:serview/ui/home_page.dart';
-//import 'package:serview/ui/sign_page.dart';
+import 'package:serview/ui/sign_page.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
