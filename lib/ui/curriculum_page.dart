53ba9d311a63e99f4356ad3dd9d2aaacf3b66df1
Anselmo Parente Martins <50084661+anselmo157@users.noreply.github.com>
2019-11-16 17:32:49 -0300

    Finalização da Tela de Currículo do Serview

--- lib/ui/curriculum_page.dart
+++ lib/ui/curriculum_page.dart
@@ -5,6 +5,28 @@ class CurriculumPage extends StatefulWidget {
   _CurriculumPageState createState() => _CurriculumPageState();
 }
 
+final String url = "https://rd1.com.br/wp-content/uploads/2019/09/20190908-rd1-alexandre-frota.png";
+final String name = "Ubireudo Silva";
+final String job = "Marceneiro";
+
+class StarDisplay extends StatelessWidget {
+  final int value;
+  const StarDisplay({Key key, this.value = 0})
+      : assert(value != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < value ? Icons.star : Icons.star_border,
+        );
+      }),
+    );
+  }
+}
+
 class _CurriculumPageState extends State<CurriculumPage> {
   @override
   Widget build(BuildContext context) {
@@ -30,7 +52,85 @@ class _CurriculumPageState extends State<CurriculumPage> {
         ],
       ),
     ),
-    
+    body: Container(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.fitHeight,
+                        image: NetworkImage(url) 
+                      )
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15.0,bottom: 5.0),
+                child:Row(
+                children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(name,
+                      style: TextStyle(fontSize: 28)
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(job,
+                      style: TextStyle(fontSize: 20)
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      StarDisplay(value: 4)
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 25.0),
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Descrição:",
+                    style: TextStyle(fontSize: 32)
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15.0),
+                child: Text("Joiner është përgjegjës profesional për të punuar me dru, ndërtimin dhe riparimin e mobiljeve, dekorative, utilitare dhe copa të tjera të drurit.",
+                style: TextStyle(fontSize: 16)
+                ),
+              ),
+            ],
+          ),
+      ],
+    ),
+  ),
   );
  }
 }
